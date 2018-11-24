947e63d226a1e1cd41a14b95653c4fbc1bfdf7fb
Puneet05 <nemadepsn@gmail.com>
2018-11-25 03:13:38 +0530

    f

--- risc_pipelining.vhd
+++ risc_pipelining.vhd
@@ -233,15 +233,15 @@ use ieee.numeric_std.all;
  stg1: stage1 
  port map (
 
- 	   clk              => clk,
-	   rst			  => rst,
-	   valid_in   => '1',
-	   pc_control   => "00",
-	   reg_b_val  =>  reg_b_val,
-	   pc_plus_imm    => pc_plus_imm,
-	   ir		  =>   ir,
-	   pc_old		  => pc_old_1,
-	   valid_out   => valid_out_1
+ 	   clk                => clk,
+	   rst			      => rst,
+	   valid_in           => '1',
+	   pc_control         => "00",
+	   reg_b_val          =>  reg_b_val,
+	   pc_plus_imm        => pc_plus_imm,
+	   ir		          =>   ir_1,
+	   pc_old		      => pc_old_1,
+	   valid_out          => valid_out_1
  	
  );
 
@@ -249,58 +249,58 @@ use ieee.numeric_std.all;
  port map (
 
  	   clk                    => clk,
-	   rst	          =>  rst,
-	   valid_in          => valid_out_1,
-	   ir         => ir,
-	   pc_old_i        => pc_old_1,
-	   carry_yes         => carry_yes_2,
-	   zero_yes        =>  zero_yes_2,
-	   pc_old_o		        => pc_old_2,
-	   imm6         => imm6_2,
-	   imm9         => imm9_2,
-	   reg_a_addr        => reg_a_addr_2,
-	   reg_b_addr        => reg_b_addr_2,
-	   reg_c_addr         => reg_c_addr_2,
-	   alu_op         =>  alu_op_2,
-	   pc_plus_imm        =>  pc_plus_imm_2,
-	   reg_addr2_ctl_3         => reg_addr2_ctl_3_2,
-	   input_alu2_ctl_4         => input_alu2_ctl_4_2,
-	   output_ctrl_4        => output_ctrl_4_2,
-	   output_ctrl_5        => output_ctrl_5_2,
-	   reg_inp_data_ctl_6        => reg_inp_data_ctl_6_2,
-	   mem_rd_5         => mem_rd_5_2,
-	   reg_wr_6         => reg_wr_6_2,
-	   jlr_yes        =>  jlr_yes_2,
-	   beq_jal_yes        => beq_jal_yes_2,
-	   valid_out        => valid_out_2
+	   rst	                  =>  rst,
+	   valid_in               => valid_out_1,
+	   ir                     => ir_1,
+	   pc_old_i               => pc_old_1,
+	   carry_yes              => carry_yes_2,
+	   zero_yes               =>  zero_yes_2,
+	   pc_old_o		          => pc_old_2,
+	   imm6                   => imm6_2,
+	   imm9                   => imm9_2,
+	   reg_a_addr             => reg_a_addr_2,
+	   reg_b_addr             => reg_b_addr_2,
+	   reg_c_addr             => reg_c_addr_2,
+	   alu_op                 =>  alu_op_2,
+	   pc_plus_imm            =>  pc_plus_imm_2,
+	   reg_addr2_ctl_3        => reg_addr2_ctl_3_2,
+	   input_alu2_ctl_4       => input_alu2_ctl_4_2,
+	   output_ctrl_4          => output_ctrl_4_2,
+	   output_ctrl_5          => output_ctrl_5_2,
+	   reg_inp_data_ctl_6     => reg_inp_data_ctl_6_2,
+	   mem_rd_5               => mem_rd_5_2,
+	   reg_wr_6               => reg_wr_6_2,
+	   jlr_yes                =>  jlr_yes_2,
+	   beq_jal_yes            => beq_jal_yes_2,
+	   valid_out              => valid_out_2
  	
  );
 
  stg3: stage3
  port map (
- 	   clk                      => clk,
-	   rst		                 => rst,
-	   valid_in                  => valid_out_2,
-	   jlr_yes                  => jlr_yes_2,
-	   beq_jal_yes                 => beq_jal_yes_2,
+ 	   clk                        => clk,
+	   rst		                  => rst,
+	   valid_in                   => valid_out_2,
+	   jlr_yes                    => jlr_yes_2,
+	   beq_jal_yes                => beq_jal_yes_2,
 
 	   reg_addr2_ctl              =>  reg_addr2_ctl_3_2,
-	   input_alu2_ctl_4             => input_alu2_ctl_4_2,
-	   output_ctrl_4                 => output_ctrl_4_2,
-	   output_ctrl_5                 => output_ctrl_5_2,
-	   reg_inp_data_ctl_6             => reg_inp_data_ctl_6_2,
-	   mem_rd_5                    =>  mem_rd_5_2,
-	   reg_wr_6                    =>  reg_wr_6_2,
+	   input_alu2_ctl_4           => input_alu2_ctl_4_2,
+	   output_ctrl_4              => output_ctrl_4_2,
+	   output_ctrl_5              => output_ctrl_5_2,
+	   reg_inp_data_ctl_6         => reg_inp_data_ctl_6_2,
+	   mem_rd_5                   =>  mem_rd_5_2,
+	   reg_wr_6                   =>  reg_wr_6_2,
 
 	   pc_old_i                   => pc_old_2,
-	   carry_yes_i                  => carry_yes_2,
+	   carry_yes_i                => carry_yes_2,
 	   zero_yes_i                 => zero_yes_2,
-	   imm6_i                      => imm6_2,
+	   imm6_i                     => imm6_2,
 	   imm9_i                     => imm9_2,
-	   reg_a_addr_i                =>reg_a_addr_2,
+	   reg_a_addr_i               =>reg_a_addr_2,
 	   reg_b_addr                 =>  reg_b_addr_2,
-	   reg_c_addr                  => reg_c_addr_2,
-	   alu_op_i                  =>   alu_op_2,
+	   reg_c_addr                 => reg_c_addr_2,
+	   alu_op_i                   =>   alu_op_2,
 	   t1                         =>  t1_3,
 	   t2                         =>  t2_3,
 	   alu_op_o                   =>  alu_op_3,
@@ -308,22 +308,22 @@ use ieee.numeric_std.all;
        imm6_o                     =>  imm6_3,
 	   imm9_o                     =>  imm9_3,
 	   reg_b_val                  => reg_b_val_3,
-	   reg_a_addr_o                => reg_a_addr_3,
-	   carry_yes_o                 => carry_yes_3,
+	   reg_a_addr_o               => reg_a_addr_3,
+	   carry_yes_o                => carry_yes_3,
 	   zero_yes_o                 =>  zero_yes_3,
        xor_comp                   =>  xor_comp_3,
                 
-       input_alu2_ctl_4_o           => input_alu2_ctl_4_3,
-	   output_ctrl_4_o              =>  output_ctrl_4_3,
-	   output_ctrl_5_o              =>  output_ctrl_5_3,
-	   reg_inp_data_ctl_6_o         => reg_inp_data_ctl_6_3,
-	   mem_rd_5_o                  =>  mem_rd_5_3,
-	   reg_wr_6_o                  =>  reg_wr_6_3,
+       input_alu2_ctl_4_o         => input_alu2_ctl_4_3,
+	   output_ctrl_4_o            =>  output_ctrl_4_3,
+	   output_ctrl_5_o            =>  output_ctrl_5_3,
+	   reg_inp_data_ctl_6_o       => reg_inp_data_ctl_6_3,
+	   mem_rd_5_o                 =>  mem_rd_5_3,
+	   reg_wr_6_o                 =>  reg_wr_6_3,
 
-	   jlr_yes_o                    =>  jlr_yes_3,
-	   beq_jal_yes_o                => beq_jal_yes_3,
+	   jlr_yes_o                  =>  jlr_yes_3,
+	   beq_jal_yes_o              => beq_jal_yes_3,
 
-	   valid_out                    =>  valid_out_3
+	   valid_out                  =>  valid_out_3
  );
 
 
@@ -331,42 +331,101 @@ use ieee.numeric_std.all;
 
  port map (
 
- 	   clk                    
-	    rst	
-	   valid_in 
-	   input_alu2_ctl 
-	   output_ctrl
-	   output_ctrl_5
-	   reg_inp_data_ctl_6
-	   mem_rd_5 
-	   reg_wr_6 
+ 	   clk                          => clk,
+	    rst	                        => rst,
+	   valid_in                     => valid_out_3,
+	   input_alu2_ctl               => input_alu2_ctl_4_3,
+	   output_ctrl                  =>    output_ctrl_4_3,
+	   output_ctrl_5                =>   output_ctrl_5_3,
+	   reg_inp_data_ctl_6           => reg_inp_data_ctl_6_3,
+	   mem_rd_5                     =>  mem_rd_5_3,
+	   reg_wr_6                     =>  reg_wr_6_3,
 
-	   reg_a_adr_in
-	   t1  
-	   t2_in  
-	   imm6 
-	   imm9 
-	   alu_op		
-	   pc_old_i		
-	   carry_yes_i 
-	   zero_yes_i
-	   reg_a_adr_out
-	   t2_out
-	   pc_old_o		
-	   alu_out 
-	   p_carry
-	   carry_yes_o 
-	   zero_yes_o
-	   p_zero 
+	   reg_a_adr_in                 =>  reg_a_addr_3,
+	   t1                           =>  t1_3,
+	   t2_in                        =>  t2_3,
+	   imm6                         =>   imm6_3,
+	   imm9                         =>   imm9_3,
+	   alu_op		                =>   alu_op_3,
+	   pc_old_i		                =>   pc_old_3,
+	   carry_yes_i                  =>    carry_yes_3,
+	   zero_yes_i                   =>   zero_yes_3,
+	   reg_a_adr_out                =>   reg_a_addr_4,
+	   t2_out                       =>  t2_4,
+	   pc_old_o		                =>   pc_old_4,
+	   alu_out                      =>   alu_out_4,
+	   p_carry                      => p_carry_4,
+	   carry_yes_o                  =>   carry_yes_4,
+	   zero_yes_o                   =>  zero_yes_4,
+	   p_zero                       =>   p_zero_4,
 
-	   output_ctrl_5_o
-	   reg_inp_data_ctl_6_o
-	   mem_rd_5_o 
-	   reg_wr_6_o 
+	   output_ctrl_5_o              =>   output_ctrl_5_4,
+	   reg_inp_data_ctl_6_o         =>   reg_inp_data_ctl_6_4,
+	   mem_rd_5_o                   =>    mem_rd_5_4,
+	   reg_wr_6_o                   =>    reg_wr_6_4,
 
-	   valid_out 
+	   valid_out                    => valid_out_4
  	
  );
+
+
+ stg5 : stage5
+ port map (
+
+	   clk                      =>  clk,
+	   rst		                =>  rst,
+	   valid_in                 =>  valid_out_4,
+	   p_carry_i                =>  p_carry_4,
+	   p_zero_i                 =>  p_zero_4,
+
+	   output_ctrl              =>  output_ctrl_5_4,
+	   read_ctrl                =>  mem_rd_5_4,
+	   reg_inp_data_ctl_6       => reg_inp_data_ctl_6_4,
+	   reg_wr_6                 =>  reg_wr_6_4,
+
+	   alu_out_5                =>  alu_out_4,
+	   reg_a_adr_in             =>  reg_a_addr_4,
+	   t2_in                    =>  t2_4,
+	   pc_old_i		            => pc_old_4,
+	   carry_yes_i              =>  carry_yes_4,
+	   zero_yes_i               =>  zero_yes_4,
+	   reg_a_adr_out            => reg_a_addr_5,
+	   t2_out                   =>  t2_5,
+	   pc_old_o		            => pc_old_5,
+	   stage_5_out              =>  stage_5_out_5,
+	   p_carry_o                =>  p_carry_5,
+	   carry_yes_o              =>  carry_yes_5,
+	   zero_yes_o               =>  zero_yes_5,
+	   p_zero_o                 =>  p_zero_5,
+
+	   reg_inp_data_ctl_6_o     =>  reg_inp_data_ctl_6_5,
+	   reg_wr_6_o               =>  reg_wr_6_5,
+
+	   valid_out                =>  valid_out_5
+ 	
+ );
+
+
+ stg6: stage6
+port map (
+
+	   clk                    =>  clk,
+	   rst		              =>  rst,
+	   valid_in               =>  valid_out_5,
+	   p_carry_i              =>  p_carry_5,
+	   p_zero_i               =>  p_zero_5,
+
+	   reg_inp_data_ctl       =>  reg_inp_data_ctl_6_5,
+	   reg_wr                 =>  reg_wr_6_5,
+
+	   stage_5_out_6          =>   stage_5_out_5,
+	   reg_a_adr_in           =>   reg_a_addr_5,
+	   pc_old_i		          =>  pc_old_5,
+	   carry_yes_i            =>   carry_yes_5,
+	   zero_yes_i             =>   zero_yes_5,
+	   valid_out              =>   valid_out_6
+	
+);
  
  	
  end architecture behave;
